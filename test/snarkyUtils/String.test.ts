581b0fc77922f78d0723843266906681ba19e54f
qcomps <coby@cobyd.com>
2022-02-12 22:22:23 -0500

    adding strings

---
+++ test/snarkyUtils/String.test.ts
@@ -0,0 +1,39 @@
+import { expect } from 'chai';
+import { StringCircuitValue } from '../../src/lib/snarkyUtils/String';
+
+import pkg from 'bs58';
+import { Field } from 'snarkyjs';
+import { UInt8 } from '../../src/lib/snarkyUtils/UInt8';
+
+const bs58 = pkg;
+
+describe('constructor', () => {
+  it('initializes an instance', () => {
+    const ztring = new StringCircuitValue('Some string');
+    expect(ztring);
+  });
+
+  it('throws an error if a string is too long to store', () => {
+    expect(() => {
+      let x = new StringCircuitValue('Some string that is too looooooooooooong');
+    }).to.throw(Error, /string cannot exceed character limit/);
+  });
+});
+
+describe('toString', () => {
+  it('returns the same string that initializes it', () => {
+    const input = 'Input String'
+    const ztring = new StringCircuitValue(input);
+    expect(ztring.toString()).to.eq(input);
+  });
+});
+
+describe('bits', () => {
+  it('can serialize and deserialize from Field', () => {
+    const input = 'Input String 123'
+    const ztring = new StringCircuitValue(input);
+    const field = Field.ofBits(ztring.toBits());
+    expect(StringCircuitValue.fromBits(ztring.toBits()).toString()).to.eq(input);
+    expect(StringCircuitValue.fromBits(field.toBits()).toString().replace(/\0/g, '')).to.eq(input);
+  });
+});
