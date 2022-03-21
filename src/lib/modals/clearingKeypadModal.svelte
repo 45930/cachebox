2893b1d1ab73838c7426ab006b63c92540df093d
qcomps <coby@cobyd.com>
2022-03-20 23:34:00 -0400

    keypad touchup

--- src/lib/modals/clearingKeypadModal.svelte
+++ src/lib/modals/clearingKeypadModal.svelte
@@ -8,7 +8,9 @@
 	export let isOpen;
 	export let onSubmit = (key: string) => {};
 
-	let numpadInput: string[] = [];
+	let i = 0;
+
+	let numpadInput: string[] = [null, null, null, null, null];
 
 	const onNumpadClick = function (value: string) {
 		switch (value) {
@@ -18,20 +20,35 @@
 				closeModal();
 				break;
 			case 'back':
-				numpadInput.pop();
+				numpadInput[i] = null;
 				numpadInput = numpadInput; // assignment in case we are doing anything reactive
+				if (i > 0) {
+					i--;
+				}
 				break;
 			default:
-				numpadInput = [...numpadInput, value];
+				numpadInput[i] = value;
+				numpadInput = numpadInput;
+				if (i < 4) {
+					i++;
+				}
 		}
 	};
 </script>
 
 {#if isOpen}
 	<div class="fixed top-10 bottom-0 right-0 left-0 flex justify-center pointer-events-none">
-		<div class="w-56 h-52 rounded-md p-4 bg-white flex flex-col justify-center pointer-events-auto">
-			<div class="flex justify-center">
-				{numpadInput.join(' ')}
+		<div class="w-64 h-60 rounded-md p-4 bg-white flex flex-col justify-center pointer-events-auto">
+			<div class="flex justify-between">
+				{#each numpadInput as char}
+					<div
+						class="w-12 h-9 pt-1 px-1 border border-solid border-slate-800 mb-3 align-middle text-center"
+					>
+						{#if char}
+							{char}
+						{/if}
+					</div>
+				{/each}
 			</div>
 			<Numpad onClick={onNumpadClick} />
 		</div>
