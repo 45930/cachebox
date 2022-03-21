99b4cb455e8d3574ed1b36646b9c0d38139eddad
qcomps <coby@cobyd.com>
2022-03-20 23:34:00 -0400

    keypad touchup

--- src/lib/utils/numpad.svelte
+++ src/lib/utils/numpad.svelte
@@ -6,19 +6,19 @@
 	<div class="flex justify-center">
 		<div
 			on:click={() => onClick('1')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			1
 		</div>
 		<div
 			on:click={() => onClick('2')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			2
 		</div>
 		<div
 			on:click={() => onClick('3')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			3
 		</div>
@@ -26,19 +26,19 @@
 	<div class="flex justify-center">
 		<div
 			on:click={() => onClick('4')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			4
 		</div>
 		<div
 			on:click={() => onClick('5')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			5
 		</div>
 		<div
 			on:click={() => onClick('6')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			6
 		</div>
@@ -46,19 +46,19 @@
 	<div class="flex justify-center">
 		<div
 			on:click={() => onClick('7')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			7
 		</div>
 		<div
 			on:click={() => onClick('8')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			8
 		</div>
 		<div
 			on:click={() => onClick('9')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			9
 		</div>
@@ -66,19 +66,19 @@
 	<div class="flex justify-center">
 		<div
 			on:click={() => onClick('back')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			&lt;
 		</div>
 		<div
 			on:click={() => onClick('0')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			0
 		</div>
 		<div
 			on:click={() => onClick('send')}
-			class="text-center w-12 h-9 border border-solid border-stone-800 bg-slate-400"
+			class="text-center w-12 h-9 pt-1 border border-solid border-stone-800 bg-slate-400"
 		>
 			OK
 		</div>
