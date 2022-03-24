f1d1fdc1d8bb4666adf3e3fee9d7a1145ac3c855
qcomps <coby@cobyd.com>
2022-03-23 22:53:06 -0400

    encryption plus more art

---
+++ src/lib/canvases/jungle.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import type { Tile } from 'src/global';
+	import { onMount } from 'svelte';
+	import jungle1_src from '$lib/assets/jungle1.jpeg';
+	import jungle2_src from '$lib/assets/jungle2.jpeg';
+	import { afterNavigate } from '$app/navigation';
+
+	let canvas: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
+
+	export let templateName: string;
+
+	let template = new Image();
+
+	afterNavigate(() => {
+		canvas = document.getElementById('canvas') as HTMLCanvasElement;
+		ctx = canvas.getContext('2d');
+
+		template.onload = function () {
+			ctx.drawImage(template, 0, 0);
+		};
+
+		if (templateName == 'jungle_path_1') {
+			template.src = jungle1_src;
+		}
+		if (templateName == 'jungle_path_2') {
+			template.src = jungle2_src;
+		}
+
+		draw();
+	});
+
+	const draw = function () {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.drawImage(template, 0, 0);
+	};
+</script>
+
+<canvas id="canvas" width="1000" height="500" />
